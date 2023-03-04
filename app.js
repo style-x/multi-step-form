1aed5645d8f5c75602f36c26c31a723fb67916d6
xan <47989552+style-x@users.noreply.github.com>
2023-03-04 21:59:51 +0100

    Buttons funken
    Muss nur noch die einzelnen Sections stylen

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const activeStep = document.querySelectorAll(".btn-next");
+const steps = document.querySelector(".steps");
+const children = steps.children;
 
-for(let i = 0; i < activeStep.length; i++) {
-  activeStep[i].addEventListener("click", function() {
-    console.log(activeStep[i].data-step);
+const sections = document.querySelectorAll("section");
+
+const btnNext = document.getElementsByClassName("btn-next");
+const btnBack = document.getElementsByClassName("btn-back");
+
+let activeStep = 0;
+
+for(let i = 0; i < btnNext.length; i++) {
+  btnNext[i].addEventListener("click", function() {
+    let s = i + 1;
+    console.log(s);
+    changeStep(s);
   })
+} 
+
+for(let i = 0; i < btnBack.length; i++) {
+  btnBack[i].addEventListener("click", function() {
+    let s = i - 1;
+    changeStep(s);
+  })
+} 
+
+function changeStep(s) {
+  console.log(children[s]);
+  if (s < 4) { 
+    children[s].classList.add("active");
+    children[activeStep].classList.remove("active");
+  }
+  sections[s].classList.add("active");
+  sections[activeStep].classList.remove("active");
+  activeStep = s;
 }
+
+/*
+console.log(activeStep);
+*/
